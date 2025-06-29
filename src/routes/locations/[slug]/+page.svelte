f4a4f205be7ffddb2d5b5a0eb7248ca17a84e22f
Rigo Rosero Castillo <rigo4791@gmail.com>
2025-06-29 11:10:48 +0900

    adds new routes for the specific items of each route

---
+++ src/routes/locations/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>Locations | Meet in Korea</title>
+	<meta
+		name="description"
+		content="a wonderfull page where you can find all the info to meet people and visit places in South Korea"
+	/>
+</svelte:head>
+
+<div>
+  a location that you selected
+</div>
