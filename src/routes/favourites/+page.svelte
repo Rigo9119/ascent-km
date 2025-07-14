894418d39e40c20c7840be0d3513e3432c1301da
Rigo Rosero Castillo <rigo4791@gmail.com>
2025-07-14 19:24:14 +0900

    adds margin bottom to the favourites title

--- src/routes/favourites/+page.svelte
+++ src/routes/favourites/+page.svelte
@@ -3,10 +3,9 @@
 </script>
 
 <div class="mx-auto max-w-7xl px-4 py-4 sm:px-6 sm:py-6 md:px-8 md:py-8">
-	<h2 class="mb-2 text-2xl font-bold text-rose-600 sm:mb-4 sm:text-3xl md:text-4xl">
+	<h2 class="md:mb-10 font-bold text-rose-600 mb-4 text-3xl md:text-4xl">
 		My Favourites
 	</h2>
-
 		<!-- Favourites: Events -->
 		<div class="mb-8">
 			<h2 class="text-lg font-bold mb-2">Favourite Events</h2>
