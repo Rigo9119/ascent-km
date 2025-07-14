67d1e7a0ab6de4d9f91764722421fd08dfc9284f
Rigo Rosero Castillo <rigo4791@gmail.com>
2025-07-14 17:47:23 +0900

    fixes the logut issue

--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -1,11 +1,7 @@
-import { error, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ locals: { supabase } }) => {
-	try {
-		await supabase.auth.signOut();
-		redirect(303, '/');
-	} catch (logoutError) {
-		throw error(404, `logout error ==> ${logoutError}`)
-	}
-}; 
+	await supabase.auth.signOut();
+	return redirect(303, '/');
+};
