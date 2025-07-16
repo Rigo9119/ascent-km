df8e2e5c3d34004c8fcb59c583351dc455340762
Rigo Rosero Castillo <rigo4791@gmail.com>
2025-07-16 16:48:43 +0900

    date range field added and styled for the project

--- src/routes/events/components/create-event-form.svelte
+++ src/routes/events/components/create-event-form.svelte
@@ -109,37 +109,40 @@
 	</createEventForm.Field>
 	<createEventForm.Field name={'date'}>
 		{#snippet children(field: AnyFieldApi)}
-			<div>
-				<DateRangeField.Root class="group flex w-full max-w-[320px] flex-col gap-1.5">
-					<DateRangeField.Label class="block text-sm font-medium select-none">
-						Date
-					</DateRangeField.Label>
-					<div class="">
-						{#each ['start', 'end'] as const as type (type)}
-							<DateRangeField.Input {type}>
-								{#snippet children({ segments })}
-									{#each segments as { part, value }, i (part + i)}
-										<div class="inline-block select-none">
-											{#if part === 'literal'}
-												<DateRangeField.Segment {part} class="text-muted-foreground p-1">
-													{value}
-												</DateRangeField.Segment>
-											{:else}
-												<DateRangeField.Segment {part} class="">
-													{value}
-												</DateRangeField.Segment>
-											{/if}
-										</div>
-									{/each}
-								{/snippet}
-							</DateRangeField.Input>
-							{#if type === 'start'}
-								<div aria-hidden="true" class="text-muted-foreground px-1">–⁠⁠⁠⁠⁠</div>
-							{/if}
-						{/each}
-					</div>
-				</DateRangeField.Root>
-			</div>
+			<DateRangeField.Root class="group flex w-full flex-col gap-1.5">
+				<DateRangeField.Label class="block text-sm font-medium select-none">
+					Hotel dates
+				</DateRangeField.Label>
+				<div
+					class="h-input rounded bg-background text-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover group-data-invalid:border-destructive flex w-full items-center border px-2 py-2 text-sm tracking-[0.01em] select-none"
+				>
+					{#each ['start', 'end'] as const as type (type)}
+						<DateRangeField.Input {type}>
+							{#snippet children({ segments })}
+								{#each segments as { part, value }, i (part + i)}
+									<div class="inline-block select-none">
+										{#if part === 'literal'}
+											<DateRangeField.Segment {part} class="text-muted-foreground p-1">
+												{value}
+											</DateRangeField.Segment>
+										{:else}
+											<DateRangeField.Segment
+												{part}
+												class="rounded-5px hover:bg-muted focus:bg-muted focus:text-foreground aria-[valuetext=Empty]:text-muted-foreground px-1 py-1 focus-visible:ring-0! focus-visible:ring-offset-0!"
+											>
+												{value}
+											</DateRangeField.Segment>
+										{/if}
+									</div>
+								{/each}
+							{/snippet}
+						</DateRangeField.Input>
+						{#if type === 'start'}
+							<div aria-hidden="true" class="text-muted-foreground px-1">–⁠⁠⁠⁠⁠</div>
+						{/if}
+					{/each}
+				</div>
+			</DateRangeField.Root>
 		{/snippet}
 	</createEventForm.Field>
 	<createEventForm.Field name={'location_id'}>
