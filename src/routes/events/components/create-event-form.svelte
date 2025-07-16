187891244c799df31a1da76bb8cec8bbccd70b18
Rigo Rosero Castillo <rigo4791@gmail.com>
2025-07-16 16:52:02 +0900

    adds the time range field

--- src/routes/events/components/create-event-form.svelte
+++ src/routes/events/components/create-event-form.svelte
@@ -7,7 +7,7 @@
 	import Label from '@/lib/components/ui/label/label.svelte';
 	import { type HtmlInputEvent } from '@/lib/types';
 	import { createForm, type AnyFieldApi, type AnyFormState } from '@tanstack/svelte-form';
-	import { Switch } from 'bits-ui';
+	import { Switch, TimeRangeField } from 'bits-ui';
 	import { DateRangeField } from 'bits-ui';
 	import { unstable_RatingGroup as RatingGroup } from 'bits-ui';
 
@@ -227,40 +227,43 @@
 	</createEventForm.Field>
 	<createEventForm.Field name={'time'}>
 		{#snippet children(field: AnyFieldApi)}
-			<div>
-				<DateRangeField.Root class="group flex w-full max-w-[320px] flex-col gap-1.5">
-					<DateRangeField.Label class="block text-sm font-medium select-none">
-						Time range
-					</DateRangeField.Label>
-					<div class="">
-						{#each ['start', 'end'] as const as type (type)}
-							<DateRangeField.Input {type}>
-								{#snippet children({ segments })}
-									{#each segments as { part, value }, i (part + i)}
-										<div class="inline-block select-none">
-											{#if part === 'literal'}
-												<DateRangeField.Segment {part} class="text-muted-foreground p-1">
-													{value}
-												</DateRangeField.Segment>
-											{:else}
-												<DateRangeField.Segment
-													{part}
-													class="rounded-5px hover:bg-muted focus:bg-muted focus:text-foreground aria-[valuetext=Empty]:text-muted-foreground px-1 py-1 focus-visible:ring-0! focus-visible:ring-offset-0!"
-												>
-													{value}
-												</DateRangeField.Segment>
-											{/if}
-										</div>
-									{/each}
-								{/snippet}
-							</DateRangeField.Input>
-							{#if type === 'start'}
-								<div aria-hidden="true" class="text-muted-foreground px-1">–⁠⁠⁠⁠⁠</div>
-							{/if}
-						{/each}
-					</div>
-				</DateRangeField.Root>
-			</div>
+        <TimeRangeField.Root class="group flex w-full flex-col gap-1.5">
+          <TimeRangeField.Label class="block select-none text-sm font-medium">
+            Event times
+          </TimeRangeField.Label>
+          <div
+            class="h-input rounded-md bg-background text-foreground focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover group-data-invalid:border-destructive flex w-full select-none items-center border px-2 py-2 text-sm tracking-[0.01em]"
+          >
+            {#each ["start", "end"] as const as type (type)}
+              <TimeRangeField.Input {type}>
+                {#snippet children({ segments })}
+                  {#each segments as { part, value }, i (part + i)}
+                    <div class="inline-block select-none">
+                      {#if part === "literal"}
+                        <TimeRangeField.Segment
+                          {part}
+                          class="text-muted-foreground p-1"
+                        >
+                          {value}
+                        </TimeRangeField.Segment>
+                      {:else}
+                        <TimeRangeField.Segment
+                          {part}
+                          class="rounded-5px hover:bg-muted focus:bg-muted focus:text-foreground aria-[valuetext=Empty]:text-muted-foreground focus-visible:ring-0! focus-visible:ring-offset-0! px-1 py-1"
+                        >
+                          {value}
+                        </TimeRangeField.Segment>
+                      {/if}
+                    </div>
+                  {/each}
+                {/snippet}
+              </TimeRangeField.Input>
+              {#if type === "start"}
+                <div aria-hidden="true" class="text-muted-foreground pl-1 pr-2">to</div>
+              {/if}
+            {/each}
+          </div>
+        </TimeRangeField.Root>
 		{/snippet}
 	</createEventForm.Field>
 	<createEventForm.Field name={'capacity'}>
