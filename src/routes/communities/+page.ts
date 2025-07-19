9b351ea8fcde0d1da2d84ab5f079a71fa46312b3
Rigo Rosero Castillo <rigo4791@gmail.com>
2025-07-19 17:30:36 +0900

    removes user from the api calls as it is in the layout

--- src/routes/communities/+page.ts
+++ src/routes/communities/+page.ts
@@ -1,14 +1,12 @@
 import type { PageLoad } from '../$types';
 
-export const load: PageLoad = async ({ fetch,  }) => {
-	const userData = await fetch('/api/user')
+export const load: PageLoad = async ({ fetch }) => {
 	const communitiesRes = await fetch('/api/communities')
 	const { publicCommunities, featuredCommunities } = await communitiesRes.json()
 
 		return {
 			communities: publicCommunities || [],
 			featuredCommunities: featuredCommunities || [],
-			user: userData
 		};
 
 };
